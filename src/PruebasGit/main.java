056ae9074de15cad0005b5eecbb27b9f3eb9fa20
CarlosRMDev <crm0027@alu.medac.es>
2024-04-08 11:19:12 +0200

    Programa de prueba
    Una simple suma, hola ale

---
+++ src/PruebasGit/main.java
@@ -0,0 +1,14 @@
+package PruebasGit;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		int a = 2;
+		
+		int suma = a + i;
+		System.out.println(suma);
+	}
+
+}
